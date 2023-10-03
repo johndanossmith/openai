210987a023b28603182f87c036866ebddd488674
NeymarRaban <albrechtraban@gmail.com>
2023-10-03 04:15:30 +0200

    openai

---
+++ chatbot.py
@@ -0,0 +1,25 @@
+import openai
+import gradio as gr
+
+openai.api_key = "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"
+
+messages = [
+    {"role": "system", "content": "You are a helpful and kind AI Assistant."},
+]
+
+def chatbot(input):
+    if input:
+        messages.append({"role": "user", "content": input})
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+        reply = chat.choices[0].message.content
+        messages.append({"role": "assistant", "content": reply})
+        return reply
+
+inputs = gr.Textbox(lines=7, label="Chat with AI")
+outputs = gr.Textbox(label="Reply")
+
+gr.Interface(fn=chatbot, inputs=inputs, outputs=outputs, title="AI Chatbot",
+             description="Ask anything you want",
+             theme="compact").launch(share=True)
